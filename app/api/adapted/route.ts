7970601e91a2db4076484cfce8e8bef6e17456f4
Felipe Domingues <felipeeu@yahoo.com.br>
2024-08-20 19:53:49 -0300

    improve api function

--- app/api/adapted/route.ts
+++ app/api/adapted/route.ts
@@ -1,5 +1,6 @@
 import { getAdeptedPrompt, getSecondExamPrompt } from "@/app/lib/prompt";
-import { GoogleGenerativeAI } from "@google/generative-ai";
+import { geminiApi } from "@/app/utils/geminiApi";
+import { GenerateContentResult } from "@google/generative-ai";
 import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
@@ -10,28 +11,17 @@ export async function GET(request: Request) {
   const level = searchParams.get("level") as string;
   const category = searchParams.get("category") as string;
   const apiKey = process.env.GEMINI_API_KEY;
+
   if (!apiKey) {
     return NextResponse.json({ error: "Missing API key" }, { status: 500 });
   }
 
-  const genAI = new GoogleGenerativeAI(apiKey);
-  const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+  const prompt = level
+    ? getSecondExamPrompt(subject, level, category, question)
+    : getAdeptedPrompt(subject, type, question);
 
-  const generationConfig = {
-    temperature: 1,
-    topP: 0.95,
-    topK: 64,
-    maxOutputTokens: 8192,
-    responseMimeType: "text/plain",
-  };
   try {
-    const chatSession = model.startChat({
-      generationConfig,
-    });
-    const prompt = level
-      ? getSecondExamPrompt(subject, level, category, question)
-      : getAdeptedPrompt(subject, type, question);
-    const result = await chatSession.sendMessage(prompt);
+    const result: GenerateContentResult = await geminiApi(prompt, apiKey);
     return NextResponse.json({ response: result.response.text() });
   } catch (error) {
     console.error("Error:", error);
